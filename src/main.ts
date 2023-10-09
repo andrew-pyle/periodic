0a89968dc8c68bd36096231cab6083a481258c6d
Andrew Pyle <andrew.pyle@iscteam.org>
2023-10-09 12:15:50 -0500

    Make app throw error if mount point is not found in the DOM

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import App from "./App.svelte";
+import "./app.css";
+
+const target = document.getElementById("app");
+
+if (!target) {
+	throw new Error(
+		'Tried to mount Svelte app at element with id="app", but that target was not found.',
+	);
+}
 
 const app = new App({
-	target: document.getElementById("app"),
+	target,
 });
 
 export default app;
